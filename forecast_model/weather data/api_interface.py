c1c2f03ea1e1bae0918de3d36dacb1f515c9093c
David S <dvstorel@ncsu.edu>
2019-01-21 13:47:42 -0500

    add generation calcs and data vis

--- forecast_model/weather data/api_interface.py
+++ forecast_model/weather data/api_interface.py
@@ -5,19 +5,31 @@ import json
 import csv
 import pandas as pd
 
-r = requests.get("https://api.solcast.com.au/radiation/estimated_actuals?longitude=120.78&latitude=-8.60&api_key=xPd9I0BI0abRZSCt8kiDisjEvFQIF7bT&format=json")
-print(r.status_code)
+def get_solcast_insolation():
+    success = False
+    r = requests.get("https://api.solcast.com.au/radiation/estimated_actuals?longitude=120.78&latitude=-8.60&api_key=xPd9I0BI0abRZSCt8kiDisjEvFQIF7bT&format=json")
+    print(r.status_code)
 
-if r.status_code == 200:
-    data = r.json()
-    data = data['estimated_actuals']
-    #print(data['estimated_actuals'])
-    #save to json file
-    #'''
-    with open('data.json', 'w') as outfile:
-        json.dump(data, outfile)
-    #'''
-    #convert to csv
-    df = pd.read_json('data.json')
-    #print(df.head())
-    df.to_csv('data.csv')
+    if r.status_code == 200:
+        success = True
+        data = r.json()
+        data = data['estimated_actuals']
+        #print(data['estimated_actuals'])
+        #save to json file
+        #'''
+        with open('data.json', 'w') as outfile:
+            json.dump(data, outfile)
+        #'''
+        #convert to csv
+        df = pd.read_json('data.json')
+        #print(df.head())
+        df.to_csv('data.csv')
+
+    return success
+
+def main():
+    s = get_solcast_insolation()
+    print('Data retrieved: '+str(s))
+
+if __name__ == '__main__':
+    main()
---
+++ forecast_model/weather data/data_vis.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 21 12:55:47 2019
+
+@author: dvsto
+"""
+
+import pandas as pd
+
+irradiance = pd.read_csv('data.csv')
+irradiance = irradiance.rename(columns={'Unnamed: 0': 'index'})
+
+#extract timestamp
+
+#calculate electricity generation
+def gen_calc(irradiance):
+    #constants
+    pannel_size = 0.42240
+    eff = 0.145
+    return irradiance*pannel_size*eff
+
+irradiance['generation_watts'] = irradiance.ghi.apply(gen_calc)
+
+#plot results
+irradiance.plot(x='index',y='generation_watts')
